3d94c53947b0d40b2ffd038e3327f5f4b3dfeee3
Pranvi Rai <21ucs157@lnmiit.ac.in>
2024-10-01 16:34:11 +0530

    first commit

---
+++ Backend/models/Booking.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const BookingSchema = new mongoose.Schema({
+  hall: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  slot: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  status: {
+    type: String,
+    default: "Pending",
+  },
+});
+
+const Booking = mongoose.model("Booking", BookingSchema);
+
+module.exports = Booking;
